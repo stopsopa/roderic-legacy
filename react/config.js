6d14bbd4fdbae5f40ad3a35ce4aac9666c24b483
Szymon Dzialowski <sd@phase-ii.com>
2017-08-08 11:01:37 +0100

    License

--- react/config.js
+++ react/config.js
@@ -1,3 +1,9 @@
+/**
+ * @author Szymon Działowski
+ * @license MIT License (c) copyright 2017-present original author or authors
+ * @homepage https://github.com/stopsopa/webpack3
+ */
+
 'use strict';
 
 const path              = require("path");
