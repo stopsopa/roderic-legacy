72f537669515d4d61c412578d3e69c9f25dbd7d6
Simon <stopsopa@users.noreply.github.com>
2017-08-24 21:45:50 +0100

    Update template.es6.js

--- template.es6.js
+++ template.es6.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link, withRouter } from 'react-router-dom';
 import { autobind } from 'core-decorators';
 import PropTypes from 'prop-types';
 import trim from 'lodash/trim';
