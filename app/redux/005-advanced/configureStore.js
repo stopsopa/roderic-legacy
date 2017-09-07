4388de2fd290274e40ad660d76e31bf36f9b1b1e
Simon <szymon.dzialowski@gmail.com>
2017-09-07 11:46:39 +0100

    Redux: Wrapping dispatch() to Log Actions https://egghead.io/lessons/javascript-redux-wrapping-dispatch-to-log-actions

--- app/redux/005-advanced/configureStore.js
+++ app/redux/005-advanced/configureStore.js
@@ -5,9 +5,33 @@ import debounce from 'lodash/debounce';
 import log from '../../../react/webpack/logw';
 import todoApp from './reducers';
 
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+
+    if (!console || !console.group) {
+
+        return rawDispatch;
+    }
+
+    return (action) => {
+        console.group(action.type);
+            log('%c action', 'color:blue', action);
+            log('%c prev', 'color:gray', store.getState());
+            const returnValue = rawDispatch(action);
+            log('%c next', 'color: green', store.getState())
+        console.groupEnd(action.type);
+        return returnValue;
+    }
+}
+
 const configureStore = () => {
 
     const store = createStore(todoApp, loadState());
+
+    if (process.env.NODE_ENV !== 'production') {
+
+        store.dispatch = addLoggingToDispatch(store);
+    }
 
     store.subscribe(debounce(() => {
 
