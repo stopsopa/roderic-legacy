5d79cf4b56de614f96eedc4f7973274ab9ebae6b
Simon <szymon.dzialowski@gmail.com>
2018-01-17 14:39:35 +0000

    links to test pages

--- app/components/Edit.js
+++ app/components/Edit.js
@@ -62,14 +62,6 @@ export default class Edit extends Component {
                                     <li><a target="_blank" href="../../router/005/app.html">/router/005/app.html</a></li>
                                     <li><a target="_blank" href="../../router/006/app.html">/router/006/app.html</a></li>
                                 </ul>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
-                                <li><a target="_blank" href="../.."></a></li>
                             </ul>
                         </td>
                     </tr>
