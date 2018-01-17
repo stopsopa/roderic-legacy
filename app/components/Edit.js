30162ff1bf7452a05f2cd2e0354cf0e8d96cf9f2
Simon <szymon.dzialowski@gmail.com>
2018-01-17 14:39:35 +0000

    links to test pages

--- app/components/Edit.js
+++ app/components/Edit.js
@@ -19,19 +19,62 @@ export default class Edit extends Component {
     }
     render = () => (
         <form>
-            <div>form...</div>
-            <div>
-                <NestedGetDataVisible />
-                <br/>
-                <a href={imgTest}>
-                    <img
-                        src={imgTest}
-                        alt="imgTest"
-                    />
-                </a>
-                <br />
-                <JustString />
-            </div>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            form...
+                            <NestedGetDataVisible />
+                            <br/>
+                            <a href={imgTest}>
+                                <img
+                                    src={imgTest}
+                                    alt="imgTest"
+                                />
+                            </a>
+                            <br />
+                            <JustString />
+                        </td>
+                        <td>
+                            <ul>
+                                <li><a target="_blank" href="../../motion/index.html">/motion/index.html</a></li>
+                                <li><a target="_blank" href="../../other/flexbox/index.html">/other/flexbox/index.html</a></li>
+                                <ul>
+                                    <li><a target="_blank" href="../../redux/001-createstore/app.html">/redux/001-createstore/app.html</a></li>
+                                    <li><a target="_blank" href="../../redux/002-createstore-own/app.html">/redux/002-createstore-own/app.html</a></li>
+                                    <li><a target="_blank" href="../../redux/003-react/app.html">/redux/003-react/app.html</a></li>
+                                    <li><a target="_blank" href="../../redux/004-counters/app.html">/redux/004-counters/app.html</a></li>
+                                    <li><a target="_blank" href="../../redux/005-advanced/app.html">/redux/005-advanced/app.html</a></li>
+                                </ul>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <ul>
+                                    <li><a target="_blank" href="../../bootstrap.html">/bootstrap.html</a></li>
+                                    <li><a target="_blank" href="../../index-dir.html">/index-dir.html</a></li>
+                                    <li><a target="_blank" href="../../react.html">/react.html</a></li>
+                                    <li><a target="_blank" href="../../scss.html">/scss.html</a></li>
+                                </ul>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <ul>
+                                    <li><a target="_blank" href="../../router/001/app.html">/router/001/app.html</a></li>
+                                    <li><a target="_blank" href="../../router/002/app.html">/router/002/app.html</a></li>
+                                    <li><a target="_blank" href="../../router/003/app.html">/router/003/app.html</a></li>
+                                    <li><a target="_blank" href="../../router/004/app.html">/router/004/app.html</a></li>
+                                    <li><a target="_blank" href="../../router/005/app.html">/router/005/app.html</a></li>
+                                    <li><a target="_blank" href="../../router/006/app.html">/router/006/app.html</a></li>
+                                </ul>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                                <li><a target="_blank" href="../.."></a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </form>
     );
 };
